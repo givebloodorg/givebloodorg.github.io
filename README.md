eec53d632611af811f8cd5580306ea0b2cbe3c09
Guilherme Chinaglia <quinalia.guilherme@gmail.com>
2017-10-24 15:20:16 -0200

    Change the structure of the gulpfile (#4)
    * Alterando arquivo gulpfile
    
    * Change structure gulpfile
    
    * Add the instructions to use the project
    
    * Add the instructions on README.me file
    
    * Add the instructions on README.me
    
    * Update file README.me
    
    * Update README.md
    
    * Update file README.ME

--- README.md
+++ README.md
@@ -2,4 +2,14 @@
 
 The official landing page for doesangue project.
 
-This webpage we will use for "**MVP**" campaign, where we plan to acquire new members and maybe future users..
+This webpage we will use for "**MVP**" campaign, where we plan to acquire new members and maybe future users.
+
+Instructions to use the project:
+
+## Follow this steps and run the commands bellow in your terminal (Linux) or cmd (Windows): ##
+
+	It's necessary to have **node.js** in your machine: https://nodejs.org/en/
+	
+	1. git clone https://github.com/doesangueorg/doesangueorg.github.io.git
+	2. <code>npm install</code> (to install the node_modules path)
+	3. run the command <code>gulp</code> in your terminal/cmd
--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,102 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var gulpConnect = require('gulp-connect');
+var gulp    = require("gulp");
+var sass    = require("gulp-sass");
+var htmlmin   = require("gulp-htmlmin");
+var notify    = require("gulp-notify");
+var concat    = require("gulp-concat");
+var uglify    = require("gulp-uglify");
+var browserSync = require("browser-sync").create();
+var del     = require("del");
+var jshint    = require("gulp-jshint");
+var cssmin    = require("gulp-cssmin");
+var runSequence = require("run-sequence");
+var imagemin  = require("gulp-imagemin");
+
+/* Tasks cached */
+gulp.task("cache:css", function() {
+  del("build/css/style.css")
+});
+
+gulp.task("cache:js", function() {
+  del("build/js/app.js")
+});
+
+gulp.task("cache:html", function(){
+  del("build/index.html")
+});
+
+/*Task minfy PNG, JPEG, GIF and SVG images*/
+gulp.task("imagemin", function(){
+  return gulp.src("source/img/**/*")
+         .pipe(imagemin())
+         .pipe(gulp.dest("build/img"));
+});
 
 
-// Task: Webserver
-// Serve the website locally using gulp-connect
-gulp.task('webserver', function(){
-  gulpConnect.server({
-    livereload: true,
-    port: '4000',
-    root: '.'
+/* Task compile scss to css */
+gulp.task("sass", ['cache:css'], function() {
+  return gulp.src("source/scss/style.scss")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest("build/css"))
+        .pipe(browserSync.stream());
+});
+
+
+/* Task minify html */
+gulp.task("html", ['cache:html'], function() {
+  return gulp.src("source/index.html")
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest("build"))
+        .pipe(browserSync.stream());
+});
+
+/*Task jshint js*/
+gulp.task("jshint", function(){
+  return gulp.src("source/js/app.js")
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+/* Task minify js */
+gulp.task("js", ['cache:js'], function() {
+  return gulp.src("source/js/app.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("build/js"))
+        .pipe(browserSync.stream());
+});
+
+/* Task concat js */
+gulp.task("concat-js", function() {
+  return gulp.src([
+          'node_modules/jquery/dist/jquery.js',
+          'node_modules/tether/dist/js/tether.js',
+          'node_modules/bootstrap/dist/js/bootstrap.js'
+        ])
+        //.pipe(concat("main.js"))
+        //.pipe(uglify())
+        .pipe(gulp.dest("build/js"))
+});
+
+/*Task move fonts to font awesome*/
+gulp.task("move-fonts", function(){
+  return gulp.src('node_modules/font-awesome/fonts/**')
+       .pipe(gulp.dest("build/fonts"))
+});
+
+/* Task server local */
+gulp.task("server", function() {
+  browserSync.init({
+    server: {
+      baseDir: "build/"
+    }
   });
+
+  /* Watch */
+  gulp.watch("source/scss/**/*.scss", ['sass']);
+  gulp.watch("node_modules/bootstrap/scss/**/*.scss", ['sass']);
+  gulp.watch("source/js/**/*.js", ['js']);
+  gulp.watch("source/index.html", ['html']);
 });
 
-// Task: Assets.
-// Minify and Concatenate the js, css, css
-gulp.task('assets', function() {
- //
+gulp.task("default", function(cb){
+  return runSequence(['imagemin', 'sass', 'html', 'jshint', 'js', 'concat-js', 'move-fonts', 'server'], cb)
 });
-// Task: Styles
-gulp.task('styles', function () {
-   return gulp.src('assets/sass/**/*.scss')
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(gulp.dest('build/css/doesangue.css'))
-    .pipe(gulpConnect.reload());
-});
-
-// Task: images. Minify images
-gulp.task('images', function() {
-   return gulp.src('assets/images/**/*{.png,.jpg,.gif,.svg}')
-     .pipe(imagefy({
-       progressive: true,
-       optimizationLevel: 5
-     }))
-     .pipe(gulp.dest('build/images/'));
-});
-
-// Wait for changes and execute the task.
-gulp.task('watch', function() {
-   gulp.watch('assets/sass/**/*.scss', ['styles'])
-   gulp.watch('assets/scripts/**/*.js', ['scripts'])
-   gulp.watch('assets/images/**/*{.png,jpg,svg,gif}', ['images'])
-});
-
-gulp.task('default', ['assets', 'watch', 'webserver']);
